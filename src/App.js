ec6313db68c44abeaf19646e1ee14fd1b2114a09
iCalculated <fufa0001@gmail.com>
2022-01-15 15:10:14 -0600

    anonymous sign in

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
 import { addDocument, collection  } from 'firebase/firestore';
-import { getAuth, singInAnonymously } from 'firebase/auth';
+import { signInAnonymously } from 'firebase/auth';
+
+import { useEffect } from 'react';
+
+import { auth } from './firebase.js';
 
 function App() {
+  useEffect(() => {
+    const a = signInAnonymously(auth);
+    console.log(a)
+  });
+
   return (
     <div className="App">
       <header className="App-header">
