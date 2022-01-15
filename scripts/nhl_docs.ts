6b3008364b5a4d32265f3538b0ffa0c0374cb94c
iCalculated <fufa0001@gmail.com>
2022-01-15 16:58:22 -0600

    types

--- scripts/nhl_docs.ts
+++ scripts/nhl_docs.ts
@@ -1,4 +1,17 @@
 
 import * as elo_history from './data/nhl/elo_history.json';
+import * as elo from './data/nhl/elo_vals.json';
+import * as team_ids from './data/nhl/team_ids.json'
+
+interface EloPoint {
+  elo: number;
+  date: string;
+}
+interface Team {
+  name: string;
+  league: string;
+  elo: number;
+  elo_history: EloPoint[]
+}
 
 console.log(elo_history)
