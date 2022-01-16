89b7a9cc09d6132b35c7f505b73fc19c1c002a72
iCalculated <fufa0001@gmail.com>
2022-01-15 18:01:51 -0600

    remove GCP firestore

--- src/App.tsx
+++ src/App.tsx
@@ -16,20 +16,29 @@ const App: React.FC = () => {
     const credential: UserCredential = await signInAnonymously(auth);
     setUser(credential.user)
     setUserId(credential.user.uid)
+    console.log(credential.user);
   }
 
   // mount loop (ideally)
   useEffect(() => {
     console.log("Setting up")
     signIn()
-    teams.forEach(team => addDoc(collection(db,"teams"), team));
   }, []);
 
+  const sendDocuments = () => {
+    console.log("Sending documents!")
+    teams.forEach(async team => {
+      const t = await addDoc(collection(db,"teams"), team)
+      console.log(await t)
+    });
+    console.log("Sent documents!")
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         Brilliant React Code
+        <button onClick={sendDocuments}> Blast off! </button>
       </header>
     </div>
   );
