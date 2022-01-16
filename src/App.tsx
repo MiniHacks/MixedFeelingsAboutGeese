4c9271bbf7cbb23ed97fbc0942018443bf7da607
iCalculated <fufa0001@gmail.com>
2022-01-15 19:51:07 -0600

    script config

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,9 @@ import React, { useEffect, useState } from 'react';
 
 import { auth } from './firebase';
 
-/*
-import { teams } from './scripts/nhl_docs'
+import { teams } from './scripts/gen_docs'
 import { setDoc, doc } from 'firebase/firestore';
 import { db } from './firebase';
-*/
 
 const App: React.FC = () => {
 
@@ -29,7 +27,6 @@ const App: React.FC = () => {
     signIn()
   }, []);
 
-  /*
   const sendDocuments = () => {
     console.log("Sending documents!")
     teams.forEach(async team => {
@@ -37,13 +34,12 @@ const App: React.FC = () => {
     });
     console.log("Sent documents!")
   }
-  <button onClick={sendDocuments}> Blast Off! </button>
-  */
 
   return (
     <div className="App">
       <header className="App-header">
         Brilliant React Code
+        <button onClick={sendDocuments}> Blast Off! </button>
       </header>
     </div>
   );
