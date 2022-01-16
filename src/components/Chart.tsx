16d83d32a3102d77badc94734ce9899d6770655f
theraggedflygon <68510342+theraggedflygon@users.noreply.github.com>
2022-01-16 12:58:57 -0600

    Playing with Chart

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -10,12 +10,12 @@ import {
   ChartOptions,
   TimeScale,
   CartesianScaleTypeRegistry,
-  ScaleOptionsByType
-} from 'chart.js';
-import 'chartjs-adapter-moment';
-import { Line } from 'react-chartjs-2';
-import { EloPoint, Team } from '../models'
-import { _DeepPartialObject } from 'chart.js/types/utils';
+  ScaleOptionsByType,
+} from "chart.js";
+import "chartjs-adapter-moment";
+import { Line } from "react-chartjs-2";
+import { EloPoint, Team } from "../models";
+import { _DeepPartialObject } from "chart.js/types/utils";
 
 ChartJS.register(
   CategoryScale,
@@ -32,56 +32,68 @@ interface Props {
 }
 
 const Chart: React.FC<Props> = (props: Props) => {
-
-  const [labels, setLabels] = useState([])
-  const [datasets, setDatasets] = useState([])
+  const [labels, setLabels] = useState([]);
+  const [datasets, setDatasets] = useState([]);
 
   useEffect(() => {
     if (props.datasets) {
-
-      const start = Math.min(...props.datasets.map(dataset => Date.parse(dataset.elo_history[0].date)))
-      const dayArray = getDaysArray(start, Date.now())
-
-      const sets = props.datasets.map(dataset => {
-        const m = new Map<number, number>();
-        dataset.elo_history.forEach( point => 
-          m.set(Date.parse(point.date), point.elo)
+      const start = Math.min(
+        ...props.datasets.map((dataset) =>
+          Date.parse(dataset.elo_history[0].date)
         )
+      );
+      const dayArray = getDaysArray(start, Date.now());
 
-        const hue = 360 * Math.random()
+      const sets = props.datasets.map((dataset) => {
+        const m = new Map<number, number>();
+        dataset.elo_history.forEach((point) =>
+          m.set(Date.parse(point.date), point.elo)
+        );
+
+        const hue = 360 * Math.random();
         return {
           label: dataset.name,
-          data: dayArray.map(day => m.get(day.getTime()) || null),
-          borderColor: `hsla(${~~(hue)},70%,70%,0.8)`,
-          backgroundColor: `hsla(${~~(hue)},70%,70%,0.8)`,
+          data: dayArray.map((day) => m.get(day.getTime()) || null),
+          borderColor: `hsla(${~~hue},70%,70%,0.8)`,
+          backgroundColor: `hsla(${~~hue},70%,70%,0.8)`,
           pointRadius: 0,
           tension: 0.3,
-          borderWidth: 5
-        }
-      })
+          borderWidth: 5,
+        };
+      });
       sets.push({
         label: "Average",
-        data: dayArray.map(day => 1400),
+        data: dayArray.map((day) => 1400),
         borderColor: `rgb(0,0,0, 0.4)`,
         backgroundColor: `rgb(0,0,0,0.4)`,
         pointRadius: 0,
         tension: 0,
-        borderWidth: 3
-      })
-      setDatasets(sets)
-      setLabels(dayArray)
+        borderWidth: 3,
+      });
+      setDatasets(sets);
+      setLabels(dayArray);
     }
-  }, [props.datasets])
-
+  }, [props.datasets]);
 
   const getDaysArray = (start, end) => {
-    for(var arr=[],dt=new Date(start); dt<=end; dt.setDate(dt.getDate()+1)){
-        arr.push(new Date(dt));
+    for (
+      var arr = [], dt = new Date(start);
+      dt <= end;
+      dt.setDate(dt.getDate() + 1)
+    ) {
+      arr.push(new Date(dt));
     }
     return arr;
+  };
+
+  return (
+    <Line
+      data={{
+        labels: labels.map((label) => label.toDateString()),
+        datasets: datasets,
+      }}
+    />
+  );
 };
 
-  return <Line data={{labels: labels.map(label => label.toDateString()), datasets: datasets}} />;
-}
-
-export default Chart
+export default Chart;
--- src/components/results.tsx
+++ src/components/results.tsx
@@ -1,35 +1,36 @@
 import React, { useState, useEffect } from "react";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../firebase";
+import { Team } from "../models";
+import Chart from "./Chart";
 
 const Results = ({ miseryScore, miseryTeams }) => {
-  const [mostMiserable, setMostMiserable] = useState({
-    teamname: null,
-    title_score: null,
-    average_elo: null,
-  });
-  const [leastMiserable, setLeastMiserable] = useState({
-    teamname: null,
-    title_score: null,
-    average_elo: null,
-  });
+  const [mostMiserable, setMostMiserable] = useState(undefined);
+  const [leastMiserable, setLeastMiserable] = useState(undefined);
 
   useEffect(() => {
     const userTeams: string[] = Object.keys(miseryTeams);
     userTeams.sort((a, b) => (miseryTeams[a] < miseryTeams[b] ? 1 : -1));
     getTeamInfo(userTeams[0], false);
     getTeamInfo(userTeams[userTeams.length - 1], true);
-  });
+  }, []);
 
   const getTeamInfo = async (teamname: string, least: boolean) => {
-    const { title_score, average_elo } = (
+    const { average_elo, name, title_score, elo_history, league, elo } = (
       await getDoc(doc(db, "team_info", teamname))
     ).data();
-    const teamObj = { teamname, title_score, average_elo };
+    const teamData = {
+      average_elo,
+      name,
+      title_score,
+      elo_history,
+      league,
+      elo,
+    };
     if (least) {
-      setLeastMiserable(teamObj);
+      setLeastMiserable(teamData);
     } else {
-      setMostMiserable(teamObj);
+      setMostMiserable(teamData);
     }
   };
 
@@ -39,15 +40,15 @@ const Results = ({ miseryScore, miseryTeams }) => {
         <h3 className="select">
           The team that makes you the most miserable is:
         </h3>
-        <h3 className="select">The {mostMiserable.teamname}</h3>
+        <h3 className="select">The {mostMiserable.name}</h3>
         <h4 className="pText">
-          The {mostMiserable.teamname} have a recent average ELO rating of{" "}
+          The {mostMiserable.name} have a recent average ELO rating of{" "}
           {mostMiserable.average_elo} when compared to the average value of
           1400.
         </h4>
-        {/* CHART GOES HERE */}
+        <Chart datasets={[mostMiserable]} />
         <h4 className="pText">
-          The {mostMiserable.teamname} have a success rating of{" "}
+          The {mostMiserable.name} have a success rating of{" "}
           {mostMiserable.title_score} since the year 2000 compared to an average
           team's value of 4400.
         </h4>
@@ -61,15 +62,15 @@ const Results = ({ miseryScore, miseryTeams }) => {
         <h3 className="select">
           The team that makes you the least miserable is:
         </h3>
-        <h3 className="select">The {leastMiserable.teamname}</h3>
+        <h3 className="select">The {leastMiserable.name}</h3>
         <h4 className="pText">
-          The {leastMiserable.teamname} have a recent average ELO rating of{" "}
+          The {leastMiserable.name} have a recent average ELO rating of{" "}
           {leastMiserable.average_elo} when compared to the average value of
           1400.
         </h4>
         {/* CHART GOES HERE */}
         <h4 className="pText">
-          The {leastMiserable.teamname} have a success rating of{" "}
+          The {leastMiserable.name} have a success rating of{" "}
           {leastMiserable.title_score} since the year 2000 compared to an
           average team's value of 4400.
         </h4>
@@ -80,8 +81,8 @@ const Results = ({ miseryScore, miseryTeams }) => {
   return (
     <div>
       <h1 className="misery">your results....</h1>
-      {mostMiserable.teamname !== null ? renderMostMiserableTeam() : null}
-      {Object.keys(miseryTeams).length > 1 && leastMiserable.teamname !== null
+      {mostMiserable !== undefined ? renderMostMiserableTeam() : null}
+      {Object.keys(miseryTeams).length > 1 && leastMiserable !== undefined
         ? renderLeastMiserableTeam()
         : null}
     </div>
