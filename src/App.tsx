b322dbcd91fb95a0e1b7ff49e47d599cbc07207b
iCalculated <fufa0001@gmail.com>
2022-01-15 18:40:54 -0600

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import React, { useEffect, useState } from 'react';
 
 import { auth } from './firebase';
 
+/*
 import { teams } from './scripts/nhl_docs'
 import { setDoc, doc } from 'firebase/firestore';
 import { db } from './firebase';
+*/
 
 const App: React.FC = () => {
 
@@ -27,6 +29,7 @@ const App: React.FC = () => {
     signIn()
   }, []);
 
+  /*
   const sendDocuments = () => {
     console.log("Sending documents!")
     teams.forEach(async team => {
@@ -34,6 +37,7 @@ const App: React.FC = () => {
     });
     console.log("Sent documents!")
   }
+  /*
 
   return (
     <div className="App">
