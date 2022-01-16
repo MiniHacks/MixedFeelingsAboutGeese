c1da451e5b2dd4efe4beb16922843c937bb16179
minizou <xiaosuantang@gmail.com>
2022-01-16 06:34:53 -0600

    Add misery index css to teams

--- src/pages/Teams.tsx
+++ src/pages/Teams.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import TeamTable from '../components/teamTable';
+let hasTeam:boolean;
+let misery:number;
+
+function getMisery(hasTeam: boolean) {
+  if (hasTeam) {
+    misery = 1231; /* FIXME - misery index calculation */
+    return ( <h1 className="misery"> Your misery index is {misery} </h1>)
+  } else {
+    return ( <h1 className="misery"> Your misery index is unknown </h1> );
+}
+};
 
 export default function Teams() {
   return (
     <div className="page teams">
+      { getMisery(hasTeam) }
       <TeamTable />
     </div>
   )
